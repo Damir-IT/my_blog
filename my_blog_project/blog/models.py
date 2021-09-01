440048a4f3149eddfe40c4fb0b82e3bcca21f1c7
Damir <akhmadulline.damir@gmail.com>
2021-09-01 13:38:32 +0300

    added blog model added blog app

--- my_blog_project/blog/models.py
+++ my_blog_project/blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class BlogPost(models.Model):
+    post_title = models.CharField(max_length=300)
+    publishing_date = models.DateTimeField(auto_now_add=True)
+    post_text = models.TextField()
+    post_image = models.ImageField(upload_to='blog_images/')
